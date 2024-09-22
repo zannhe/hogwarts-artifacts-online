23a7fa72e4ddcf616431827c86407728cadc4223
zannhe <zannhe@gmail.com>
2024-09-22 16:17:56 +0800

    首次提交

---
+++ src/main/java/com/zannhe/hogwartsartifactsonline/HogwartsArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.zannhe.hogwartsartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsArtifactsOnlineApplication.class, args);
+	}
+
+}
